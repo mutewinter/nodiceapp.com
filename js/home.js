00dd5a3a3912a8655a290ce1a1e7ab57dbb891b9
Jeremy Mack <mack@heysparkbox.com>
2013-10-15 20:10:41 -0400

    feat: Initial commit

---
+++ js/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("a.single_image").fancybox({
+  'overlayShow' : true,
+  'overlayColor' : "#111"
+  });
+});
+
+(function() {
+  var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+  uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/Qeu6wdSMb1HpgH0hP6BjA.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
